446c4912114aaba3a65998bdf7a95b2bac3fa7fa
Hunter Motte <huntermotte@gmail.com>
2017-03-11 16:04:58 -0500

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+var recipePuppyAPI = 'http://www.recipepuppy.com/api/'
+
+function getDataFromAPI(ingredients, callback) {
+  var query = {
+    i: ingredients
+  }
+  $.getJSON(recipePuppyAPI, query, callback)
+}
+
+function displayRecipes(data) {
+  var resultElement = ''
+  if (data.results) {
+    data.results.forEach(function(result) {
+      console.log(result);
+      resultElement += '<p>' + '<a href=' + '"' + result.href + '"' + '>' + result.title + '</a></p>' + '<p>' + result.ingredients + '</p>' + '<p>' + result.href + '</p>'
+    });
+  }
+  else {
+    resultElement += '<p>No results</p>';
+  }
+  $('.search-results').html(resultElement);
+}
+
+function watchSubmit() {
+  $('.searchbar').submit(function(event) {
+    event.preventDefault();
+    var query = $(this).find('.query').val();
+    getDataFromAPI(query, displayRecipes)
+  });
+}
+    
+$(function(){watchSubmit();})
