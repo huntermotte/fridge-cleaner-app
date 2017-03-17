f3681951ea4186026dfb5c2672b317cb18daf202
Hunter Motte <huntermotte@gmail.com>
2017-03-16 20:12:08 -0400

    this better work

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ function getDataFromAPI(ingredients, callback) {
   url: recipePuppyAPI + '?/i=' + ingredients,
   method: 'GET',
   headers: {
-  'Access-Control-Allow-Origin': 'https://huntermotte.github.io'
+  'Access-Control-Allow-Origin': 'https://huntermotte.github.io/Capstone-FEWD/'
   },
   success: function(response) {
   callback(response)
